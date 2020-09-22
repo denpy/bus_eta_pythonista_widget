b1e46418e50c5444c80da4f60166d19e3ec52ed8
denpy <2denmiro@gmail.com>
2020-09-22 12:19:29 +0300

    Small refactoring, improved comments

--- bus_eta_widget.py
+++ bus_eta_widget.py
@@ -9,17 +9,18 @@ from contextlib import contextmanager
 from itertools import cycle
 from timeit import default_timer
 
+# Pythonista internal modules
 # noinspection PyUnresolvedReferences
-import appex  # Pythonista internal module
+import appex
 import geopy.distance
 # noinspection PyUnresolvedReferences
-import location  # Pythonista internal module
+import location
 # noinspection PyUnresolvedReferences
-import ui  # Pythonista internal module
+import ui
 
 # TODO: This should be changed to "config" when all is ready
-from bus_eta_pythonista_widget.my_config import DATA_REFRESH_INTERVAL, NO_ETA_MESSAGE, STATIONS_DATA
-from bus_notify.bus_eta_notifier import BusEtaNotifier
+from bus_eta_pythonista_widget.my_config import DATA_REFRESH_INTERVAL, NO_ETA_MESSAGE, STATIONS_DATA, STATUS_LABELS
+from bus_notify.bus_arrivals_notifier import BusArrivalsNotifier
 
 # Set level for urllib3 logger so we will not see any messages on widget, otherwise its logs will appear on the widget
 # background
@@ -58,16 +59,16 @@ class DummyLogger(object):
         return lambda *x: None
 
 
-class MyEtaNotifier(BusEtaNotifier):
+class MyArrivalsNotifier(BusArrivalsNotifier):
 
     def __init__(self):
         dl = DummyLogger()
-        super(MyEtaNotifier, self).__init__(dl)
+        super(MyArrivalsNotifier, self).__init__(dl)
         self.station_id = None
 
-    def _get_station_data(self, station_id: int):
+    def _get_station_info(self, station_id: int):
         start = default_timer()
-        res_json = super(MyEtaNotifier, self)._get_station_data(self.station_id)
+        res_json = super(MyArrivalsNotifier, self)._get_station_info(self.station_id)
         self.get_station_data_elapsed = round(default_timer() - start)
         return res_json
 
@@ -88,18 +89,22 @@ class MyEtaNotifier(BusEtaNotifier):
 
     def _make_eta_msg(self, status_label):
         msgs = []
-        station_name = self.etas['station_name']
-        station_city = self.etas['station_city']
 
         # Bus station details
-        station_label = STATIONS_DATA[self.station_id].get('label', '🚏')
-        station_details_str = f'{station_city} - {station_name} {station_label} {status_label}'.center(42, ' ')
+        station_city = self.arrivals['station_city']
+        station_label = STATIONS_DATA[self.station_id].get('label')
+        station_name = self.arrivals['station_name'] if station_label is None else station_label
+        station_details_str = f'{station_city} - {station_name} {status_label}'.center(42, ' ')
 
         # ETAs text
-        line_number_2_etas = self.etas['line_number_2_etas']
+        line_number_2_etas = self.arrivals['line_num_2_mins_remained']
         for line_number, etas in sorted(line_number_2_etas.items(), key=lambda item: item[0]):  # sort by line number
             etas_str = ', '.join([str(eta) if eta > 0 else 'Now' for eta in line_number_2_etas[line_number]])
             msg = f'🚌 {line_number}: {etas_str}'
+
+            # TODO: Currently the alignment will work with iPhone X and later. Make it work with any iPhone model
+            #  with no relation to the screen size
+            # Align text, so it will appear in the middle of of the widget
             msg = f'{msg: <25}'.center(60, ' ')
             msgs.append(msg)
 
@@ -111,7 +116,7 @@ class MyEtaNotifier(BusEtaNotifier):
 
     def send_notification(self):
         """
-        Present ETAs in Pythonista's today widget
+        Present bus arrivals in Pythonista's today widget
         """
         # Configure the iOS today widget to present the text in dynamic view i.e. the size of the widget will be
         # dynamic and can be expanded when text can't fit the default widget size
@@ -125,9 +130,12 @@ class MyEtaNotifier(BusEtaNotifier):
         # every one second the number of refreshes will as a number of seconds we need to wait between Curlbus
         # data requests
         widget_refresh_count = DATA_REFRESH_INTERVAL - self.get_station_data_elapsed
-        for status_label in cycle(('⬆️', '⬇️')):
+        for status_label in cycle(STATUS_LABELS):
             widget_refresh_count -= 1
             should_request_etas = widget_refresh_count == 0
+
+            # When it's time to query Curlbus again we change the status label so it will show that data query is
+            # performed
             if should_request_etas is True:
                 status_label = '🔄'
             msg = self._make_eta_msg(status_label)
@@ -142,5 +150,5 @@ class MyEtaNotifier(BusEtaNotifier):
 
 
 if __name__ == '__main__':
-    my_notifier = MyEtaNotifier()
+    my_notifier = MyArrivalsNotifier()
     my_notifier.run(DATA_REFRESH_INTERVAL)
